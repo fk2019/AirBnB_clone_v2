39d9fe5a9addab274415a6daff708e290d9c9def
fk2019 <fkmuiruri8@gmail.com>
2023-04-23 05:30:04 +0000

    Add location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,6 @@ echo "<html>
      </html>" > /data/web_static/releases/test/index.html
 ln -s /data/web_static/releases/tests/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-#file="/etc/nginx/sites-available/default"
-#sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' "$file"
-sed -i '/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
+file="/etc/nginx/sites-available/default"
+sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' "$file"
 service nginx restart
