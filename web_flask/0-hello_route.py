78e68b1fdcbd56ac1e7e36853a9a17cd924de995
fk2019 <fkmuiruri8@gmail.com>
2023-04-24 18:18:05 +0000

    Add route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,13 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes = False)
 def hello_route():
     """Display Hello HBNB in / route"""
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
