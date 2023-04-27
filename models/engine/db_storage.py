ee49fbcc9f12a8cb1cc38a148738a16b1e306c30
fk2019 <fkmuiruri8@gmail.com>
2023-04-27 18:18:20 +0000

    Add close

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,3 +76,7 @@ class DBStorage():
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Terminate current db session"""
+        self.__session.close()
